91a9e5f0664d52409da59a759487cb3f98d53cae
joseperezdmgz <joseperezdmgz@gmail.com>
2024-12-26 00:04:16 +0100

    refactor: update app layout and add header and footer components; configure runtime settings and add mock database

---
+++ components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer class="fixed bottom-0 w-full bg-gray-800 text-white text-center p-4">
+        <p>&copy; {{ new Date().getFullYear() }} <a href="https://joseperezdmgz.es/" target="_blank" class="hover:text-gray-300">José Pérez Domínguez</a></p>
+    </footer>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="flex flex-col gap-4 text-xl w-3/4 mx-auto py-4">
+        <div class="flex justify-between">
+            <NuxtLink to="/">Joseperezdmgz</NuxtLink>
+            <a href="https://github.com/joseperezdmgz/project-repo" target="_blank">Github</a>
+        </div>
+        <nav class="flex gap-4">
+            <template v-for="item in menu">
+                <NuxtLink :to="item.path" class="hover:text-gray-500 hover:border-b-2" activeClass="border-b-2 border-black transition-all duration-300">{{ item.name }}</NuxtLink>
+            </template>
+        </nav>
+    </header>
+    <hr/>
+</template>
+
+<script setup>
+    const menu = ref([
+        { name:'Home', path: '/' },
+        { name:'Customers', path: '/customers' },
+        { name:'Products', path: '/products' },
+    ])
+</script>
